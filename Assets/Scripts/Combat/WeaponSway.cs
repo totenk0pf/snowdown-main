f117c672353c415f5253127eb032fb0248e6f240
Huu Quang Nguyen <50345722+totenk0pf@users.noreply.github.com>
2023-06-09 11:35:50 +0700

    ngfsfgsf

--- Assets/Scripts/Combat/WeaponSway.cs
+++ Assets/Scripts/Combat/WeaponSway.cs
@@ -4,19 +4,22 @@ namespace Combat {
     public class WeaponSway : MonoBehaviour {
         [SerializeField] private float horizontalSway;
         [SerializeField] private float verticalSway;
+        [SerializeField] private float horizontalAngleClamp;
+        [SerializeField] private float verticalAngleClamp;
         [SerializeField] private float smoothing;
         [SerializeField] private Transform swayTransform;
 
-        private void Update() {
+        private void LateUpdate() {
             Vector2 input = new(
-                Input.GetAxisRaw("Mouse X") * horizontalSway,
-                Input.GetAxisRaw("Mouse Y") * verticalSway
+                Mathf.Clamp(Input.GetAxisRaw("Mouse X") * horizontalSway, -horizontalAngleClamp, horizontalAngleClamp),
+                Mathf.Clamp(Input.GetAxisRaw("Mouse Y") * verticalSway, -verticalAngleClamp, verticalAngleClamp)
             );
+
             Quaternion rotX = Quaternion.AngleAxis(-input.y, Vector3.right);
             Quaternion rotY = Quaternion.AngleAxis(input.x, Vector3.up);
-            Quaternion targetRot = rotX * rotY;
+            Quaternion targetRot = targetRot = rotX * rotY;
 
-            swayTransform.localRotation = Quaternion.Slerp(transform.localRotation, targetRot, smoothing * Time.deltaTime);
+            swayTransform.localRotation = Quaternion.Slerp(swayTransform.localRotation, targetRot, smoothing * Time.deltaTime);
         }
     }
 }
